3338f0c3641d1f586fa2f024f70c055c4ebff304
space7z <702010343@qq.com>
2023-05-13 16:03:51 +0800

    home结构拆分以及分类数据渲染

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,11 +1,20 @@
 <script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import HomeProduct from './components/HomeProduct.vue'
 
 </script>
 
 <template>
-  <div class="home" style="height:1200px">
-    ws
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
   </div>
+  <HomeNew />
+  <import />
+  <HomeProduct />
 </template>
 
 <style scoped lang="scss">
