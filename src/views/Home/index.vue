05dbdde0b531b95532929189c6b6b081442c79d1
space7z <702010343@qq.com>
2023-05-14 10:16:05 +0800

    home-panel组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 
 </script>
 
@@ -15,6 +16,12 @@ import HomeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <import />
   <HomeProduct />
+  <HomePanel title="新鲜好物">
+    <div>新鲜好物</div>
+  </HomePanel>
+  <HomePanel title="人气推荐">
+    <div>人气推荐</div>
+  </HomePanel>
 </template>
 
 <style scoped lang="scss">
